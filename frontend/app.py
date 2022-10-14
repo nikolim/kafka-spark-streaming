1269c91f55dfa9991c124e86a90e70e85922c22a
Nikolai Limbrunner <ntli@kth.se>
2022-10-14 17:31:21 +0200

    Added btc endpoint

--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,21 @@
+from concurrent.futures import thread
 import time
 from flask import Flask
+from kafka import KafkaConsumer
+from threading import Thread
 
 app = Flask(__name__)
 
-@app.route('/time')
+curr_btc_price = [0]
+
+def consume():
+    consumer = KafkaConsumer('raw', bootstrap_servers='localhost:9092')
+    for message in consumer:
+        curr_btc_price[0] = message.value.decode('utf-8')
+
+consume_thread = Thread(target=consume)
+consume_thread.start()  
+
+@app.route('/btc')
 def get_current_time():
-    return {'time': time.time()}
+    return {'price': curr_btc_price[0]}
--- frontend/react-frontend/src/App.js
+++ frontend/react-frontend/src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [btcPrice, setBtcPrice] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setBtcPrice(data.price);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>The current bitcoin price is {btcPrice}.</p>
       </header>
     </div>
   );
