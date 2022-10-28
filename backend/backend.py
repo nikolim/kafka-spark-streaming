6e4276b2128cdfe67dcf5201db29d495eea522c6
AbyelT <abielo.tesfay@gmail.com>
2022-10-28 13:07:57 +0200

    testing new endpoints

--- backend/backend.py
+++ backend/backend.py
@@ -6,16 +6,24 @@ import requests
 KAFKA_BROKER = os.environ['KAFKA_BROKER']  if "KAFKA_BROKER" in os.environ else "localhost:9092"
 
 producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER)
-binanceAPI = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+binanceAPI_BTC = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT" #can probably have an list of symbols
+binanceAPI_ETH = "https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT"
+
+## some other apis
+# price and hash rate: https://api.minerstat.com/v2/coins?list=BTC
 
 while True:
 	try:
-		data = requests.get(binanceAPI)
-		data = data.json()
-		bitcoin_price = data["price"]
-		future = producer.send('raw', str(bitcoin_price).encode('utf-8'))
-		print("Backend sent: " + str(bitcoin_price))
-		future.get(timeout=60)
-		sleep(0.1)
+		data_BTC = requests.get(binanceAPI_BTC)
+		data_ETH = requests.get(binanceAPI_ETH)
+
+		data1 = data_BTC.json()
+		data2 = data_ETH.json()
+
+		price = {"BTC": data1["price"], "ETH": data2["price"]}
+		# future = producer.send('raw', str(price).encode('utf-8'))
+		print("Backend sent: " + str(price))
+		# future.get(timeout=60)
+		sleep(1)
 	except KeyboardInterrupt:
 		break
