be302fda9d8c8a7dca936142b561ee1a754fa3ea
niko <nikolai.limbrunner@gmail.com>
2022-10-28 17:28:22 +0200

    Added some more sample plots to the frontend

--- b4f/frontend/src/App.js
+++ b4f/frontend/src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import { useEffect, useState } from 'react';
 import Plot from './components/plot';
-import './App.css';
 import io from 'socket.io-client';
 
 function App() {
@@ -14,15 +12,32 @@ function App() {
 
   useEffect(() => {
     const socket = io('http://localhost:5000');
-      socket.on('btc', (data) => appendBtcPrice(data.price)
-    )}
-   ,[]);
+    socket.on('btc', (data) => appendBtcPrice(data.price)
+    )
+  }
+    , []);
+
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Plot data={btcPrices} />
-      </header>
+    <div className="App h-screen bg-gray-800 grid place-items-center">
+      <h1 className="text-5xl text-white">Bitcoin Streaming Correlation</h1>
+      <div className="flex">
+        <div className="flex flex-col items-center max-w-[30%]">
+          <h1 className="text-4xl text-white"> Bitcoin price</h1>
+          <Plot data={btcPrices} color={"#1a237e"} />
+          <div className="text-xl text-white"> {btcPrices[btcPrices.length - 1]}$</div>
+        </div>
+        <div className="flex flex-col items-center max-w-[30%]">
+          <h1 className="text-4xl text-white"> Hashrate </h1>
+          <Plot data={btcPrices} color={"#7b1fa2"} />
+          <div className="text-xl text-white"> {btcPrices[btcPrices.length - 1]} hash/unit</div>
+        </div>
+        <div className="flex flex-col items-center max-w-[30%]">
+          <h1 className="text-4xl text-white"> Correlation </h1>
+          <Plot data={btcPrices} color={"#7b1fa2"} />
+          <div className="text-xl text-white"> {btcPrices[btcPrices.length - 1]} </div>
+        </div>
+      </div>
     </div>
   );
 }
